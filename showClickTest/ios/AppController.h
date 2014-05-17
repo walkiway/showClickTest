3adf2201ddf2a2a51dd9c74399451e7e371c7466
colin <hitwhylz@163.com>
2014-05-17 08:06:39 +0800

    Initial Commit

---
+++ showClickTest/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  showClickTestAppController.h
+//  showClickTest
+//
+//  Created by Colin on 14-5-17.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ showClickTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  showClickTestAppController.h
+//  showClickTest
+//
+//  Created by Colin on 14-5-17.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
